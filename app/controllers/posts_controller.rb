82816653e3be95a969efc059a56094fd3d808cfe
Sam Bricking <sambricking@gmail.com>
2014-01-21 00:40:31 +0000

    5.6

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,20 @@ class PostsController < ApplicationController
   end
   
   def create
-    render text: params[:post].inspect
+    @post = Post.new(params[:post])
+    @post.save
+    redirect_to @post
   end
   
+
+
+
+  
+  private
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
+
 end
+
+
